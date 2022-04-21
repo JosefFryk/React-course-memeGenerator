c8b62bdd6d5d5123739226dd0b48c153f2af2c8c
Josef Fryk <josef.fryk@gmail.com>
2022-04-21 21:25:38 +0200

    text update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -29,7 +29,25 @@ export default function Meme() {
         })
         )
 
-}
+    }
+    function handleChange(event){
+        const {name,value} = event.target
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+
+    }
+
+
+    function handleSubmit(event){
+        event.preventDefault();
+  
+    }
+
 
     return (
         <main>
@@ -37,22 +55,34 @@ export default function Meme() {
                 <input 
                     type="text"
                     placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                     
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                     
                 />
                 <button 
                     onClick={getRandomImage}
+                    className="form--button"
+                    onSubmit={handleSubmit}
                 >
                     Get a new meme image 🖼
                 </button>
             </div>
 
-            <div>
+            <div className="meme">
                 <img src={meme.randomImage} alt=""  className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
            
         </main>
